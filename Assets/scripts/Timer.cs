21ffed92cd0db0b75576f2150fbcb0a70aabf02d
ianyourgod <ianyourgod@gmail.com>
2026-02-28 12:18:14 -0600

    timer

---
+++ Assets/scripts/Timer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text time;
+
+    float currentTime = 0;
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+        string sec_space = seconds < 10 ? "0" : "";
+        string min_space = minutes < 10 ? "0" : "";
+        time.text = $"{min_space}{minutes}:{sec_space}{seconds}";
+    }
+}
